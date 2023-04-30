33552e2fdbdc7e19b4f0e0a0525fff5fded7e60f
Paul Schofield <paul.schofield.us@gmail.com>
2023-04-30 12:02:57 -0400

    project desc, todo, add links and thumbnail

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,19 +4,16 @@ export default function Projects() {
   return (
     <main className='h-screen w-full flex flex-col'>
       <Navbar></Navbar>
-      <div className='container mx-auto'>
-        <div className='col-span-1 text-center w-3/5 mx-auto mt-20'>
-          <h1 className='text-4xl font-bold'>Loren Ipsum</h1>
-          <p className='mt-10'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
-            justo purus. Donec molestie ante eu nulla vehicula, quis dignissim
-            risus malesuada. Maecenas laoreet faucibus lectus, sit amet.
-          </p>
-          <br />
-          <p>
-            Phasellus ornare magna auctor urna sagittis, id ullamcorper velit
-            accumsan. Sed id tortor justo. Integer sollicitudin, ipsum eget
-            euismod convallis, sapien felis semper ante, in suscipit urna leo
+      <div className='container mx-auto text-left'>
+        <div className='col-span-1 w-3/5 mx-auto mt-20'>
+          <h1 className='text-3xl font-bold'>Howlr</h1>
+          <p className='mt-5'>
+            A social applicaiton designed to connect pet owners with local
+            events, dog sitters, and other pet-friendly resources. The app is a
+            two-sided marketplace and features the ability to connect with
+            others, discover relevant events and services for one's pets. I
+            integrated with the Google Maps API design and implemented API
+            routes for pet sitters. The project was build with...
           </p>
         </div>
       </div>
