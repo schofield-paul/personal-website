e584791ecf71bde425bbe7bcb968be7da0c2314a
Paul Schofield <paul.schofield.us@gmail.com>
2023-04-29 20:53:02 -0400

    Change column layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,37 @@ export default function Home() {
   return (
     <main className="h-screen w-full flex flex-col">
       <div className="flex justify-between items-center">
-        <div className="flex justify-left items-center">
-          <p className="text-gray-800 font-medium text-lg mt-5 ml-3">
+        <div className="flex items-center mr-auto">
+          <p className="text-gray-800 font-medium text-lg mt-5 ml-5">
             Paul Schofield
           </p>
         </div>
         <div className="flex justify-right items-center">
           <p className="text-gray-800 font-medium text-lg mt-5 mr-6">About</p>
+          <p className="text-gray-800 font-medium text-lg mt-5 mr-6">
+            Projects
+          </p>
         </div>
       </div>
-      <div className="flex flex-row h-full w-full">
-        <div className="flex-1 p-8 text-center">
-          <h1 className="text-4xl font-bold pt-4">Hello 👋</h1>
-          <p className="mt-4">I'm Paul</p>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-1 flex justify-center items-center">
+          <div className="rounded-full overflow-hidden p-6">
+            <Image
+              className="rounded-full"
+              src={profilePic}
+              alt="My Photo"
+              width={205}
+              height={205}
+            />
+          </div>
         </div>
-        <div className="flex-1 flex justify-center items-center mt-4 mr-5 mt-[-500px]">
-          <div className="rounded-full overflow-hidden">
-            <Image src={profilePic} alt="My Photo" width={225} height={225} />
+        <div className="col-span-2 text-center text-left">
+          <h1 className="text-4xl font-bold pt-7 ml-5 mt-6">Hello 👋</h1>
+          <p className="text-xl mt-4 mr-8 ">I'm Paul</p>
+          <div className="">
+            <p className="text-xl mt-6 mr-10">
+              I'm a software engineer living in New York.
+            </p>
           </div>
         </div>
       </div>
