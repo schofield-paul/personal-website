b41a48f3cd6b49330c0a2344cf31b6e660c0c16d
Paul Schofield <paul.schofield.us@gmail.com>
2023-04-30 11:16:40 -0400

    projects page layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
-/** @format */
-
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex items-center mr-auto">
-        <p className="text-gray-800 font-medium text-lg mt-5 ml-5">
+    <div className='flex justify-between items-center'>
+      <div className='flex items-center mr-auto'>
+        <p className='text-gray-800 font-medium text-lg mt-5 ml-5'>
           Paul Schofield
         </p>
       </div>
-      <div className="flex justify-right items-center">
-        <p className="text-gray-800 font-medium text-lg mt-5 mr-6">
-          <Link href="/">About</Link>
+      <div className='flex justify-right items-center'>
+        <p className='text-gray-800 font-medium text-lg mt-5 mr-6'>
+          <Link href='/'>About</Link>
         </p>
-        <p className="text-gray-800 font-medium text-lg mt-5 mr-6">
-          <Link href="/projects">Projects</Link>
+        <p className='text-gray-800 font-medium text-lg mt-5 mr-6'>
+          <Link href='/projects'>Projects</Link>
         </p>
       </div>
     </div>
