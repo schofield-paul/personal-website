e028ea2d071b2ca7e9b3b2e8377a65dbf42f8d8a
Paul Schofield <paul.schofield.us@gmail.com>
2023-05-01 16:48:50 -0400

    homepage content and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ export default function Navbar() {
   return (
     <div className='flex justify-between items-center'>
       <div className='flex items-center mr-auto'>
-        <p className='text-gray-800 font-medium text-lg mt-5 ml-5'>
-          Paul Schofield
+        <p className='text-gray-800 font-medium text-lg mt-5 ml-5 hover:text-blue-400 transition duration-200 ease-in-out'>
+          <Link href='/'>Paul Schofield</Link>
         </p>
       </div>
       <div className='flex justify-right items-center'>
-        <p className='text-gray-800 font-medium text-lg mt-5 mr-6'>
+        <p className='text-gray-800 font-medium text-lg mt-5 mr-6 hover:text-blue-400 transition duration-200 ease-in-out'>
           <Link href='/'>About</Link>
         </p>
-        <p className='text-gray-800 font-medium text-lg mt-5 mr-6'>
+        <p className='text-gray-800 font-medium text-lg mt-5 mr-6 hover:text-blue-400 transition duration-200 ease-in-out'>
           <Link href='/projects'>Projects</Link>
         </p>
       </div>
