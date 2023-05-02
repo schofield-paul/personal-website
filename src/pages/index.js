f656edfab69c591fa64b77a46c2b6cb0e44736b3
Paul Schofield <paul.schofield.us@gmail.com>
2023-05-02 13:01:20 -0400

    col-flex, breakpoint and padding for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,13 @@ import Image from 'next/image';
 import Navbar from '../components/Navbar';
 import profilePic from 'src/profilePic.png';
 import Link from 'next/link';
-import GithubContributionGraph from '../components/GithubContributionGraph';
 
 export default function Home() {
   return (
-    <main className="h-screen w-full flex flex-col">
+    <main className="h-screen w-full flex flex-col bg-white">
       <Navbar />
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1 flex justify-center items-center">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row">
+        <div className="md:w-2/5 flex justify-center mt-10">
           <div className="rounded-full overflow-hidden p-6">
             <Image
               className="rounded-full"
@@ -20,12 +19,10 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="col-span-2 text-center lg:text-left">
-          <h1 className="text-4xl font-bold pt-7 ml-5 mt-6">
-            Hi! I&apos;m Paul 👋
-          </h1>
-          <div className="mt-8 lg:mt-10 ml-5 mr-10">
-            <p className="text-xl">
+        <div className="md:w-3/5 flex flex-col justify-center p-5">
+          <h1 className="text-4xl font-bold pt-7">Hi! I&apos;m Paul 👋</h1>
+          <div className="mt-8">
+            <p className="text-xl w-full md:w-3/5">
               I&apos;m a Software Engineer based in Brooklyn, NY. Prior to
               engineering, I worked in dispute operations at{' '}
               <Link
@@ -39,7 +36,7 @@ export default function Home() {
               , a card issuer processor.
             </p>
             <br />
-            <p className="text-xl">
+            <p className="text-xl w-full md:w-3/5">
               You can find me on{' '}
               <Link
                 href="https://www.linkedin.com/in/paul-schofield-io/"
@@ -61,10 +58,9 @@ export default function Home() {
               .
             </p>
             <br />
-            <p className="text-xl">
-              Finally, I get a ton of value out of books and conversations —
-              feel free to connect and or share high-impact content from your
-              bookshelf.
+            <p className="text-xl w-full md:w-3/5">
+              I get a ton of value out of books and conversations — feel free to
+              connect and or share high-impact content from your bookshelf.
             </p>
             <br />
             <br />
