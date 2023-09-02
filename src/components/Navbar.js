40827dd64908c228a0a36f320cc71ef2a3e77715
Paul Schofield <paul.schofield.us@gmail.com>
2023-09-02 14:01:55 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center bg-white">
+    <div className="flex justify-between items-center">
       <div className="flex items-center mr-auto">
         <p className="text-gray-800 font-medium text-lg mt-5 ml-5 hover:text-blue-400 transition duration-200 ease-in-out">
           <Link href="/">Paul Schofield</Link>
